4d5cf2d5f0f105e96aecbd45193e523c5db40ac3
Prodigitaly Technologies <50889173+prodigitaly@users.noreply.github.com>
2024-03-27 18:26:42 +0530

    Update stripe.js

--- utility/setup/stripe.js
+++ utility/setup/stripe.js
@@ -1,6 +1,3 @@
-const stripeKey =
-  process.env.NODE_ENV === "production"
-    ? "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"
-    : "sk_test_51MGvznGi7bwABort1GkoMw0gP2OhxTaDTPgl0H49MNOxE2MSGB4PaQPbxhMBO7haNC3CfVnIEQlr1VxxXTNCl64f000JIV2KNx";
+const stripeKey = "sk_test_51MGvznGi7bwABort1GkoMw0gP2OhxTaDTPgl0H49MNOxE2MSGB4PaQPbxhMBO7haNC3CfVnIEQlr1VxxXTNCl64f000JIV2KNx";
 
 module.exports = require("stripe")(stripeKey);
